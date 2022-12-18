c95a6139830a856fe1a458c19678a45962f98dd9
tudortudose <tudor.tudose31@gmail.com>
2022-12-18 21:08:53 +0200

    mysql - create table & insert statements

--- mysql_manager.py
+++ mysql_manager.py
@@ -16,4 +16,30 @@ def connect(host, user, password, database):
     return database
 
 
+def create_files_table(database):
+    cursor = database.cursor()
+    cursor.execute(
+        'CREATE TABLE files(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL UNIQUE ,'
+        ' location VARCHAR(255) NOT NULL UNIQUE , encryption_alg VARCHAR(255) NOT NULL , key_size INT NOT NULL )')
+
+
+def insert_file(database, file_info):
+    cursor = database.cursor()
+    sql_statement = 'INSERT INTO files (name, location, encryption_alg, key_size) ' \
+                    'VALUES(%s, %s, %s, %s)'
+    sql_values = (file_info["name"], file_info["location"],
+                  file_info["encryption_alg"], file_info["key_size"])
+    print(sql_values)
+    cursor.execute(sql_statement, sql_values)
+    db.commit()
+
+
 db = connect(HOST, USER, PASSWORD, DATABASE)
+create_files_table(db)
+insert_file(db,
+            {
+                "name": "file1",
+                "location": "C:/Users/tudor/Desktop/file1.txt",
+                "encryption_alg": "RSA",
+                "key_size": 1024
+            })
